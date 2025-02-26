b4bd05f5cba8b96581c70d4587a281c01d8be7f1
John Doe <binod@neptunemutual.com>
2025-02-26 13:10:46 +0530

    Bump version to 1.0.2 and update tests; add changelog for release

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,4 +8,4 @@ from anyparser_core import __version__
 def test_version():
     """Test version is a string."""
     assert isinstance(__version__, str)
-    assert __version__ == "1.0.1"
+    assert __version__ == "1.0.2"
